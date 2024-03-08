d2ebe2a1d0ffaaf56db98f56e27ebb7703ff788a
Mercy <mainawanjirumercy@gmail.com>
2024-03-08 17:43:37 +0300

    first commit

---
+++ src/pages/Frame.js
@@ -0,0 +1,47 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <div className="wireframe-1-parent">
+      <div className="wireframe-1">
+        <img className="wireframe-1-child" alt="" src="/rectangle-1@2x.png" />
+        <div className="wireframe-1-item" />
+        <div className="wireframe-1-inner" />
+        <img
+          className="resumo-do-logotipo-do-nibus-"
+          alt=""
+          src="/resumo-do-logotipo-do-nibus---vetor-premium-1@2x.png"
+        />
+        <div className="rectangle-div" />
+        <div className="safari-coach">
+          <p className="safari">SaFaRi</p>
+          <p className="safari">CoAcH</p>
+        </div>
+        <img className="instagram-1-icon" alt="" src="/instagram-1@2x.png" />
+        <img
+          className="twitter-sign-1-icon"
+          alt=""
+          src="/twittersign-1@2x.png"
+        />
+        <img className="facebook-1-icon" alt="" src="/facebook-1@2x.png" />
+        <i className="join-the-community">Join the community</i>
+        <i className="seamless-journeys-effortless-container">
+          <p className="safari">Seamless Journeys, Effortless Bookings</p>
+          <p className="safari">
+            Your Pathway to Hassle-Free Travel with Safari Couch.
+          </p>
+        </i>
+        <img className="user-1-icon" alt="" src="/user-1@2x.png" />
+        <img className="road-1-icon" alt="" src="/road-1@2x.png" />
+        <div className="book-bus">BOOK BUS</div>
+        <div className="contact-us">contact us</div>
+        <div className="about-us">About us</div>
+        <div className="wireframe-1-child1" />
+        <div className="wireframe-1-child2" />
+        <div className="book-now">BOOK NOW</div>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
